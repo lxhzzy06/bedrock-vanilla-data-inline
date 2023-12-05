3e810f35b247fc0231c5bc99caf5e340ff8af342
lxhzzy06 <86933476+lxhzzy06@users.noreply.github.com>
2023-12-05 09:44:05 +0000

    via update

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,11 +1,19 @@
 import gulp from 'gulp';
+import { deleteAsync } from 'del';
+import rename from 'gulp-rename';
 
-function test(cb) {
-	console.log('test');
-	cb();
+function change_file_ext() {
+	return gulp.src('pkg/lib/*.d.ts').pipe(
+		rename((path) => {
+			path.basename = path.basename.replace('.d', '');
+			path.extname = '.ts';
+		})
+	);
 }
 
-function b() {}
+function delete_file() {
+	return deleteAsync(['pkg/lib/*.{d.ts,js}']);
+}
 
-export default test;
+export default gulp.series(change_file_ext, delete_file);
 
