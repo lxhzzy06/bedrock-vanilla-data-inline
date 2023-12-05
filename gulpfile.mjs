4a83917861fbca14c2f3bfb3ca3d0d111ce48846
lxhzzy <lxhzzy@outlook.com>
2023-12-05 20:01:03 +0800

    setup gulpfile

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,19 +1,36 @@
 import gulp from 'gulp';
 import { deleteAsync } from 'del';
 import rename from 'gulp-rename';
+import jeditor from 'gulp-json-editor';
+import replace from 'gulp-replace';
 
 function change_file_ext() {
-	return gulp.src('pkg/lib/*.d.ts').pipe(
-		rename((path) => {
-			path.basename = path.basename.replace('.d', '');
-			path.extname = '.ts';
-		})
-	);
+	return gulp.src('pkg/lib/*.d.ts').pipe(rename((path) => (path.basename = path.basename.replace('.d', ''))));
 }
 
 function delete_file() {
 	return deleteAsync(['pkg/lib/*.{d.ts,js}']);
 }
 
-export default gulp.series(change_file_ext, delete_file);
+function editor(path, mergeWith) {
+	return gulp.src(path, { base: '.' }).pipe(jeditor(mergeWith)).pipe(gulp.dest('.'));
+}
 
+function edit_package() {
+	return editor('pkg/package.json', {
+		name: 'bedrock-vanilla-data-inline',
+		description: '',
+		contributors: [{ name: 'lxhzzy', email: 'lxhzzy@outlook.com' }],
+		types: undefined,
+		exports: './lib/index.ts'
+	});
+}
+
+function set_constant_enum() {
+	return gulp
+		.src('pkg/lib/*.ts', { base: '.' })
+		.pipe(replace('export declare enum', 'export const enum', { skipBinary: false }))
+		.pipe(gulp.dest('.'));
+}
+
+export default gulp.series(edit_package, change_file_ext, delete_file, set_constant_enum);
